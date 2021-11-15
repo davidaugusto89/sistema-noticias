de68d51f6dd112816a72698e3b6b7ca23d36f5e4
David Augusto <57589195+davidaugusto89@users.noreply.github.com>
2021-11-15 17:19:46 -0300

    Ajuste página acessar

--- resources/views/noticias/acessar.blade.php
+++ resources/views/noticias/acessar.blade.php
@@ -6,7 +6,6 @@
 <div class="row">
     <div class="col-12">
         <h2>{{ $noticia->titulo }}</h2>
-        <small class="pt-1 pb-3 d-block">Data de publicação: {{ date('d/m/Y H:i:s', strtotime($noticia->created_at)) }}</small>
         <p>{!! nl2br(e($noticia->noticia)) !!}</p>
         <hr class="my-4"/>
         Categoria: <a href="{{ route('noticias.index') }}?search={{ $noticia->categoria }}" class="badge badge-light">{{ $noticia->categoria }}</a>
